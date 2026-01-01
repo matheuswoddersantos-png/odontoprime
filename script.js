470ee3874df0d051991805207ae3e34ffbf5ee66
matheuswoddersantos-png <matheuswoddersantos@gmail.com>
2026-01-01 15:51:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function mostrarMensagem() {
-    alert("Entre em contato pelo Whatapp: (11) 99999-9999");
-    
-}
+const elements = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, { threshold: 0.2 });
+
+elements.forEach(el => {
+    el.style.opacity = 0;
+    el.style.transform = "translateY(40px)";
+    el.style.transition = "all 0.8s ease";
+    observer.observe(el);
+});
